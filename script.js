678f063c519b03f6f445f8c1b7cbde6480899f38
hydraahmad1234567890-ship-it <hydraahmad1234567890@gmail.com>
2026-03-17 14:55:08 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// JavaScript for your Spotify Ultra clone
+console.log("Spotify Ultra script loaded!");
+
+// Example: Search functionality placeholder
+const searchInput = document.querySelector('.search-container input');
+searchInput.addEventListener('input', (e) => {
+    console.log("Searching for:", e.target.value);
+});
